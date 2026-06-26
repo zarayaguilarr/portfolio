859eff5373e67daf430a620b00d4cec70670ae1f
Zaray Aguilar <zaguilar@ucsd.edu>
2026-06-26 12:52:01 -0700

    Test: DarkModeProvider + About only

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { DarkModeProvider, useDarkMode } from './contexts/DarkModeContext'
-import Navigation from './components/section/Navigation'
+import { DarkModeProvider } from './contexts/DarkModeContext'
 import About from './components/section/About'
-import Footer from './components/Footer'
-
-function Content() {
-  const { isDarkMode } = useDarkMode();
-  return (
-    <>
-      <Navigation />
-      <div style={{ backgroundColor: isDarkMode ? '#1a1a1a' : '#fff' }}>
-        <About />
-        <Footer />
-      </div>
-    </>
-  )
-}
 
 function App() {
   return (
     <DarkModeProvider>
-      <Content />
+      <About />
     </DarkModeProvider>
   )
 }
