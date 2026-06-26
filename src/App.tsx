03fd62577213ec4feb88dab08ef71956f7b532e2
Zaray Aguilar <zaguilar@ucsd.edu>
2026-06-26 12:29:33 -0700

    Add Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
-import { DarkModeProvider } from './contexts/DarkModeContext'
+import { DarkModeProvider, useDarkMode } from './contexts/DarkModeContext'
+import Navigation from './components/section/Navigation'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import './App.css'
 
 function HomePage() {
-  return <div style={{ padding: '20px' }}>Home Page</div>
+  return <div style={{ padding: '20px', marginTop: '80px' }}>Home Page</div>
+}
+
+function AppContent() {
+  const { isDarkMode } = useDarkMode();
+
+  return (
+    <>
+      <Navigation />
+      <div style={{ backgroundColor: isDarkMode ? '#101727' : '#fff' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
 function App() {
   return (
     <ErrorBoundary>
       <DarkModeProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <AppContent />
       </DarkModeProvider>
     </ErrorBoundary>
   )
